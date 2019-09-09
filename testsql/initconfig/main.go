445320239f1f44e31d46f9080a430ec52d6263d4
3308200258@qq.com <3308200258@qq.com>
2019-09-09 15:43:24 +0800

    ...

--- testsql/initconfig/main.go
+++ testsql/initconfig/main.go
@@ -46,96 +46,10 @@ var (
 	version = "0.4.5"
 	isforce = 1
 
-	btcoddssy = "BTC"
-	btcoddsUpodds = 0.9
-	btcoddsDownodds = 0.9
-
-	shcioddssy = "SHCI"
-	shcioddsUpodds = 0.9
-	shcioddsDownodds = 0.9
-
-	szcioddssy = "SZCI"
-	szcioddsUpodds = 0.9
-	szcioddsDownodds = 0.9
-
-
-	btcioisy = "BTC"
-	btcoiLevelOne = 1
-	btcoiOneMindv = float64(0)
-	btcoiOneMaxdv = float64(50000)
-	btcoiOneGreaterodds = 0.9
-	btcoiOneLessodds = 0.9
-
-	btcoiLevelTwo = 2
-	btcoiTwoMindv = float64(50000)
-	btcoiTwoMaxdv = float64(100000)
-	btcoiTwoGreaterodds = 1.2
-	btcoiTwoLessodds = 0.6
-
-	btcoiLevelThree = 3
-	btcoiThreeMindv = float64(100000)
-	btcoiThreeMaxdv = float64(200000)
-	btcoiThreeGreaterodds = 1.7
-	btcoiThreeLessodds = 0.1
-
-	btcoiLevelFour = 4
-	btcoiFourMindv = float64(200000)
-	btcoiFourGreaterodds = 1.8
-	btcoiFourLessodds = float64(0)
-
-	shcioisy = "SHCI"
-	shcioiLevelOne = 1
-	shcioiOneMindv = float64(0)
-	shcioiOneMaxdv = float64(50000)
-	shcioiOneGreaterodds = 0.9
-	shcioiOneLessodds = 0.9
-
-	shcioiLevelTwo = 2
-	shcioiTwoMindv = float64(50000)
-	shcioiTwoMaxdv = float64(100000)
-	shcioiTwoGreaterodds = 1.2
-	shcioiTwoLessodds = 0.6
-
-	shcioiLevelThree = 3
-	shcioiThreeMindv = float64(100000)
-	shcioiThreeMaxdv = float64(200000)
-	shcioiThreeGreaterodds = 1.7
-	shcioiThreeLessodds = 0.1
-
-	shcioiLevelFour = 4
-	shcioiFourMindv = float64(200000)
-	shcioiFourGreaterodds = 1.8
-	shcioiFourLessodds = float64(0)
-
-	szcioisy = "SZCI"
-	szcioiLevelOne = 1
-	szcioiOneMindv = float64(0)
-	szcioiOneMaxdv = float64(50000)
-	szcioiOneGreaterodds = 0.9
-	szcioiOneLessodds = 0.9
-
-	szcioiLevelTwo = 2
-	szcioiTwoMindv = float64(50000)
-	szcioiTwoMaxdv = float64(100000)
-	szcioiTwoGreaterodds = 1.2
-	szcioiTwoLessodds = 0.6
-
-	szcioiLevelThree = 3
-	szcioiThreeMindv = float64(100000)
-	szcioiThreeMaxdv = float64(200000)
-	szcioiThreeGreaterodds = 1.7
-	szcioiThreeLessodds = 0.1
-
-	szcioiLevelFour = 4
-	szcioiFourMindv = float64(200000)
-	szcioiFourGreaterodds = 1.8
-	szcioiFourLessodds = float64(0)
 )
 
 
 func main() {
-
-
 
 	mysql.AddOneRecord(&mysql.Subject{Symbol:btcsy,Type:btctype,Isopen:btcisopen,Firstopenhour:btcFirstopenhour,
 		Firstopenmin:btcFirstopenmin,Firstclosehour:btcFirstclosehour,Firstclosemin:btcFirstclosemin})
@@ -150,37 +64,6 @@ func main() {
 
 	mysql.AddOneRecord(&mysql.Clientversion{Version:version,Isforce:isforce,Createtime:time.Now().Unix()})
 
-	mysql.AddOneRecord(&mysql.Odds{Symbol:btcoddssy,Upodds:btcoddsUpodds,Downodds:btcoddsDownodds})
-	mysql.AddOneRecord(&mysql.Odds{Symbol:shcioddssy,Upodds:shcioddsUpodds,Downodds:shcioddsDownodds})
-	mysql.AddOneRecord(&mysql.Odds{Symbol:szcioddssy,Upodds:szcioddsUpodds,Downodds:szcioddsDownodds})
-
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:btcioisy,Level:btcoiLevelOne,Mindv:btcoiOneMindv,Maxdv:btcoiOneMaxdv,
-		Greaterodds:btcoiOneGreaterodds,Lessodds:btcoiOneLessodds})
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:btcioisy,Level:btcoiLevelTwo,Mindv:btcoiTwoMindv,Maxdv:btcoiTwoMaxdv,
-		Greaterodds:btcoiTwoGreaterodds,Lessodds:btcoiTwoLessodds})
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:btcioisy,Level:btcoiLevelThree,Mindv:btcoiThreeMindv,Maxdv:btcoiThreeMaxdv,
-		Greaterodds:btcoiThreeGreaterodds,Lessodds:btcoiThreeLessodds})
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:btcioisy,Level:btcoiLevelFour,Mindv:btcoiFourMindv,
-		Greaterodds:btcoiFourGreaterodds,Lessodds:btcoiFourLessodds})
-
-
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:shcioisy,Level:shcioiLevelOne,Mindv:shcioiOneMindv,Maxdv:shcioiOneMaxdv,
-		Greaterodds:shcioiOneGreaterodds,Lessodds:shcioiOneLessodds})
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:shcioisy,Level:shcioiLevelTwo,Mindv:shcioiTwoMindv,Maxdv:shcioiTwoMaxdv,
-		Greaterodds:shcioiTwoGreaterodds,Lessodds:shcioiTwoLessodds})
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:shcioisy,Level:shcioiLevelThree,Mindv:shcioiThreeMindv,Maxdv:shcioiThreeMaxdv,
-		Greaterodds:shcioiThreeGreaterodds,Lessodds:shcioiThreeLessodds})
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:shcioisy,Level:shcioiLevelFour,Mindv:shcioiFourMindv,
-		Greaterodds:shcioiFourGreaterodds,Lessodds:shcioiFourLessodds})
-
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:szcioisy,Level:szcioiLevelOne,Mindv:szcioiOneMindv,Maxdv:szcioiOneMaxdv,
-		Greaterodds:szcioiOneGreaterodds,Lessodds:szcioiOneLessodds})
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:szcioisy,Level:szcioiLevelTwo,Mindv:szcioiTwoMindv,Maxdv:szcioiTwoMaxdv,
-		Greaterodds:szcioiTwoGreaterodds,Lessodds:szcioiTwoLessodds})
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:szcioisy,Level:szcioiLevelThree,Mindv:szcioiThreeMindv,Maxdv:szcioiThreeMaxdv,
-		Greaterodds:szcioiThreeGreaterodds,Lessodds:szcioiThreeLessodds})
-	mysql.AddOneRecord(&mysql.OddsInfo{Symbol:szcioisy,Level:szcioiLevelFour,Mindv:szcioiFourMindv,
-		Greaterodds:szcioiFourGreaterodds,Lessodds:szcioiFourLessodds})
 
 	mysql.AddOneRecord(&mysql.Depositway{Way:1,Isopen:1})
 	mysql.AddOneRecord(&mysql.Depositway{Way:2,Isopen:0})
---
+++ testsql/initredis/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"gotest/testsql/redis"
+	"log"
+	"strconv"
+)
+
+var(
+	BTCOddsInfo = "BTCOddsInfo"
+	SHCIOddsInfo = "SHCIOddsInfo"
+	SZCIOddsInfo = "SZCIOddsInfo"
+
+	BTCLevelOneMaxDv = 50000
+	BTCLevelOneGreaterOdds = 0.9
+	BTCLevelOneLessOdds = 0.9
+
+	BTCLevelTwoMaxDv = 100000
+	BTCLevelTwoGreaterOdds = 1.2
+	BTCLevelTwoLessOdds = 0.6
+
+	BTCLevelThreeMaxDv = 200000
+	BTCLevelThreeGreaterOdds = 1.7
+	BTCLevelThreeLessOdds = 0.1
+
+	BTCLevelFourGreaterOdds = 1.8
+	BTCLevelFourLessOdds = 0
+
+	BTCMAP = map[string]interface{}{
+		"LevelOneMaxDv":BTCLevelOneMaxDv, "LevelOneGreaterOdds":BTCLevelOneGreaterOdds,"LevelOneLessOdds":BTCLevelOneLessOdds,
+
+		"LevelTwoMaxDv":BTCLevelTwoMaxDv, "LevelTwoGreaterOdds":BTCLevelTwoGreaterOdds,"LevelTwoLessOdds":BTCLevelTwoLessOdds,
+
+		"LevelThreeMaxDv":BTCLevelThreeMaxDv, "LevelThreeGreaterOdds":BTCLevelThreeGreaterOdds,"LevelThreeLessOdds":BTCLevelThreeLessOdds,
+
+		"LevelFourGreaterOdds":BTCLevelFourGreaterOdds,"LevelFourLessOdds":BTCLevelFourLessOdds,
+	}
+
+	SHCILevelOneMaxDv = 50000
+	SHCILevelOneGreaterOdds = 0.9
+	SHCILevelOneLessOdds = 0.9
+
+	SHCILevelTwoMaxDv = 100000
+	SHCILevelTwoGreaterOdds = 1.2
+	SHCILevelTwoLessOdds = 0.6
+
+	SHCILevelThreeMaxDv = 200000
+	SHCILevelThreeGreaterOdds = 1.7
+	SHCILevelThreeLessOdds = 0.1
+
+	SHCILevelFourGreaterOdds = 1.8
+	SHCILevelFourLessOdds = 0
+
+	SHCIMAP = map[string]interface{}{
+		"LevelOneMaxDv":SHCILevelOneMaxDv, "LevelOneGreaterOdds":SHCILevelOneGreaterOdds,"LevelOneLessOdds":SHCILevelOneLessOdds,
+
+		"LevelTwoMaxDv":SHCILevelTwoMaxDv, "LevelTwoGreaterOdds":SHCILevelTwoGreaterOdds,"LevelTwoLessOdds":SHCILevelTwoLessOdds,
+
+		"LevelThreeMaxDv":SHCILevelThreeMaxDv, "LevelThreeGreaterOdds":SHCILevelThreeGreaterOdds,"LevelThreeLessOdds":SHCILevelThreeLessOdds,
+
+		"LevelFourGreaterOdds":SHCILevelFourGreaterOdds,"LevelFourLessOdds":SHCILevelFourLessOdds,
+	}
+
+	SZCILevelOneMaxDv = 50000
+	SZCILevelOneGreaterOdds = 0.9
+	SZCILevelOneLessOdds = 0.9
+
+	SZCILevelTwoMaxDv = 100000
+	SZCILevelTwoGreaterOdds = 1.2
+	SZCILevelTwoLessOdds = 0.6
+
+	SZCILevelThreeMaxDv = 200000
+	SZCILevelThreeGreaterOdds = 1.7
+	SZCILevelThreeLessOdds = 0.1
+
+	SZCILevelFourGreaterOdds = 1.8
+	SZCILevelFourLessOdds = 0
+
+	SZCIMAP = map[string]interface{}{
+		"LevelOneMaxDv":SZCILevelOneMaxDv, "LevelOneGreaterOdds":SZCILevelOneGreaterOdds,"LevelOneLessOdds":SZCILevelOneLessOdds,
+
+		"LevelTwoMaxDv":SZCILevelTwoMaxDv, "LevelTwoGreaterOdds":SZCILevelTwoGreaterOdds,"LevelTwoLessOdds":SZCILevelTwoLessOdds,
+
+		"LevelThreeMaxDv":SZCILevelThreeMaxDv, "LevelThreeGreaterOdds":SZCILevelThreeGreaterOdds,"LevelThreeLessOdds":SZCILevelThreeLessOdds,
+
+		"LevelFourGreaterOdds":SZCILevelFourGreaterOdds,"LevelFourLessOdds":SZCILevelFourLessOdds,
+	}
+
+	BTCCurOdds = "BTCCurOdds"
+	SHCICurOdds = "SHCICurOdds"
+	SZCICurOdds = "SZCICurOdds"
+
+	BTCCURMAP = map[string]interface{}{"UpOdds":0.9,"DownOdds":0.9,"Count":0}
+	SHCICURMAP = map[string]interface{}{"UpOdds":0.9,"DownOdds":0.9,"Count":0}
+	SZCICURMAP = map[string]interface{}{"UpOdds":0.9,"DownOdds":0.9,"Count":0}
+)
+
+func main() {
+
+	redis.HPOddsHMSet(BTCOddsInfo,BTCMAP)
+	redis.HPOddsHMSet(SHCIOddsInfo,SHCIMAP)
+	redis.HPOddsHMSet(SZCIOddsInfo,SZCIMAP)
+	redis.HPOddsHMSet(BTCCurOdds,BTCCURMAP)
+	redis.HPOddsHMSet(SHCICurOdds,SHCICURMAP)
+	redis.HPOddsHMSet(SZCICurOdds,SZCICURMAP)
+
+	btccurodds := redis.HPOddsHGetAll(BTCCurOdds)
+	log.Printf("%+v\n",btccurodds)
+	count,_ := strconv.Atoi(btccurodds["Count"])
+	UpOdds,_ := strconv.ParseFloat(btccurodds["UpOdds"],64)
+	DownOdds,_ := strconv.ParseFloat(btccurodds["DownOdds"],64)
+	log.Printf("%+v,%+v,%+v\n",count,UpOdds,DownOdds)
+
+	btcoddsinfo := redis.HPOddsHGetAll(BTCOddsInfo)
+	LevelOneMaxDv,_ := strconv.ParseFloat(btcoddsinfo["LevelOneMaxDv"],64)
+	LevelOneGreaterOdds,_ := strconv.ParseFloat(btcoddsinfo["LevelOneGreaterOdds"],64)
+	LevelOneLessOdds,_ := strconv.ParseFloat(btcoddsinfo["LevelOneLessOdds"],64)
+	LevelTwoMaxDv,_ := strconv.ParseFloat(btcoddsinfo["LevelTwoMaxDv"],64)
+	LevelTwoGreaterOdds,_ := strconv.ParseFloat(btcoddsinfo["LevelTwoGreaterOdds"],64)
+	LevelTwoLessOdds,_ := strconv.ParseFloat(btcoddsinfo["LevelTwoLessOdds"],64)
+	LevelThreeMaxDv,_ := strconv.ParseFloat(btcoddsinfo["LevelThreeMaxDv"],64)
+	LevelThreeGreaterOdds,_ := strconv.ParseFloat(btcoddsinfo["LevelThreeGreaterOdds"],64)
+	LevelThreeLessOdds,_ := strconv.ParseFloat(btcoddsinfo["LevelThreeLessOdds"],64)
+	LevelFourGreaterOdds,_ := strconv.ParseFloat(btcoddsinfo["LevelFourGreaterOdds"],64)
+	LevelFourLessOdds,_ := strconv.ParseFloat(btcoddsinfo["LevelFourLessOdds"],64)
+	log.Printf("%+v\n",btcoddsinfo)
+	log.Printf("%+v,%+v,%+v\n%+v,%+v,%+v\n%+v,%+v,%+v\n%+v,%+v\n",
+		LevelOneMaxDv,LevelOneGreaterOdds,LevelOneLessOdds,
+		LevelTwoMaxDv,LevelTwoGreaterOdds,LevelTwoLessOdds,
+		LevelThreeMaxDv,LevelThreeGreaterOdds,LevelThreeLessOdds,
+		LevelFourGreaterOdds,LevelFourLessOdds)
+
+}
