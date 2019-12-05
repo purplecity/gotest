4b772ef9c522805aa5dfe1387e8bf9ac964bff53
3308200258@qq.com <3308200258@qq.com>
2019-12-05 12:30:18 +0800

    Update main.go

--- csvtest/btcdepth/main.go
+++ csvtest/btcdepth/main.go
@@ -38,6 +38,8 @@ func main() {
 		wsConn,_,err := hpdial.Dial(BTCBaseURL,nil)
 		if err != nil {
 			log.Printf("ERROR----dial binance ws failed----err:%+v\n",err)
+			time.Sleep(time.Second*1)
+			continue
 		}
 		doneC := make(chan struct{})
 		go func() {
@@ -50,51 +52,52 @@ func main() {
 					log.Printf("ERROR----read binance message failed----err:%+v\n",err.Error())
 					return
 				}
-				json.Unmarshal(message,&Event)
-				w.Write([]string{fmt.Sprintf("%+v",time.Now().Unix()),
-					Event["bids"].([]interface{})[0].([]interface{})[0].(string),
-					Event["bids"].([]interface{})[1].([]interface{})[0].(string),
-					Event["bids"].([]interface{})[2].([]interface{})[0].(string),
-					Event["bids"].([]interface{})[3].([]interface{})[0].(string),
-					Event["bids"].([]interface{})[4].([]interface{})[0].(string),
-					Event["bids"].([]interface{})[5].([]interface{})[0].(string),
-					Event["bids"].([]interface{})[6].([]interface{})[0].(string),
-					Event["bids"].([]interface{})[7].([]interface{})[0].(string),
-					Event["bids"].([]interface{})[8].([]interface{})[0].(string),
-					Event["bids"].([]interface{})[9].([]interface{})[0].(string),
-					Event["asks"].([]interface{})[0].([]interface{})[0].(string),
-					Event["asks"].([]interface{})[1].([]interface{})[0].(string),
-					Event["asks"].([]interface{})[2].([]interface{})[0].(string),
-					Event["asks"].([]interface{})[3].([]interface{})[0].(string),
-					Event["asks"].([]interface{})[4].([]interface{})[0].(string),
-					Event["asks"].([]interface{})[5].([]interface{})[0].(string),
-					Event["asks"].([]interface{})[6].([]interface{})[0].(string),
-					Event["asks"].([]interface{})[7].([]interface{})[0].(string),
-					Event["asks"].([]interface{})[8].([]interface{})[0].(string),
-					Event["asks"].([]interface{})[9].([]interface{})[0].(string),
-					Event["bids"].([]interface{})[0].([]interface{})[1].(string),
-					Event["bids"].([]interface{})[1].([]interface{})[1].(string),
-					Event["bids"].([]interface{})[2].([]interface{})[1].(string),
-					Event["bids"].([]interface{})[3].([]interface{})[1].(string),
-					Event["bids"].([]interface{})[4].([]interface{})[1].(string),
-					Event["bids"].([]interface{})[5].([]interface{})[1].(string),
-					Event["bids"].([]interface{})[6].([]interface{})[1].(string),
-					Event["bids"].([]interface{})[7].([]interface{})[1].(string),
-					Event["bids"].([]interface{})[8].([]interface{})[1].(string),
-					Event["bids"].([]interface{})[9].([]interface{})[1].(string),
-					Event["asks"].([]interface{})[0].([]interface{})[1].(string),
-					Event["asks"].([]interface{})[1].([]interface{})[1].(string),
-					Event["asks"].([]interface{})[2].([]interface{})[1].(string),
-					Event["asks"].([]interface{})[3].([]interface{})[1].(string),
-					Event["asks"].([]interface{})[4].([]interface{})[1].(string),
-					Event["asks"].([]interface{})[5].([]interface{})[1].(string),
-					Event["asks"].([]interface{})[6].([]interface{})[1].(string),
-					Event["asks"].([]interface{})[7].([]interface{})[1].(string),
-					Event["asks"].([]interface{})[8].([]interface{})[1].(string),
-					Event["asks"].([]interface{})[9].([]interface{})[1].(string),
+				if message != nil {
+					json.Unmarshal(message,&Event)
+					w.Write([]string{fmt.Sprintf("%+v",time.Now().Unix()),
+						Event["bids"].([]interface{})[0].([]interface{})[0].(string),
+						Event["bids"].([]interface{})[1].([]interface{})[0].(string),
+						Event["bids"].([]interface{})[2].([]interface{})[0].(string),
+						Event["bids"].([]interface{})[3].([]interface{})[0].(string),
+						Event["bids"].([]interface{})[4].([]interface{})[0].(string),
+						Event["bids"].([]interface{})[5].([]interface{})[0].(string),
+						Event["bids"].([]interface{})[6].([]interface{})[0].(string),
+						Event["bids"].([]interface{})[7].([]interface{})[0].(string),
+						Event["bids"].([]interface{})[8].([]interface{})[0].(string),
+						Event["bids"].([]interface{})[9].([]interface{})[0].(string),
+						Event["asks"].([]interface{})[0].([]interface{})[0].(string),
+						Event["asks"].([]interface{})[1].([]interface{})[0].(string),
+						Event["asks"].([]interface{})[2].([]interface{})[0].(string),
+						Event["asks"].([]interface{})[3].([]interface{})[0].(string),
+						Event["asks"].([]interface{})[4].([]interface{})[0].(string),
+						Event["asks"].([]interface{})[5].([]interface{})[0].(string),
+						Event["asks"].([]interface{})[6].([]interface{})[0].(string),
+						Event["asks"].([]interface{})[7].([]interface{})[0].(string),
+						Event["asks"].([]interface{})[8].([]interface{})[0].(string),
+						Event["asks"].([]interface{})[9].([]interface{})[0].(string),
+						Event["bids"].([]interface{})[0].([]interface{})[1].(string),
+						Event["bids"].([]interface{})[1].([]interface{})[1].(string),
+						Event["bids"].([]interface{})[2].([]interface{})[1].(string),
+						Event["bids"].([]interface{})[3].([]interface{})[1].(string),
+						Event["bids"].([]interface{})[4].([]interface{})[1].(string),
+						Event["bids"].([]interface{})[5].([]interface{})[1].(string),
+						Event["bids"].([]interface{})[6].([]interface{})[1].(string),
+						Event["bids"].([]interface{})[7].([]interface{})[1].(string),
+						Event["bids"].([]interface{})[8].([]interface{})[1].(string),
+						Event["bids"].([]interface{})[9].([]interface{})[1].(string),
+						Event["asks"].([]interface{})[0].([]interface{})[1].(string),
+						Event["asks"].([]interface{})[1].([]interface{})[1].(string),
+						Event["asks"].([]interface{})[2].([]interface{})[1].(string),
+						Event["asks"].([]interface{})[3].([]interface{})[1].(string),
+						Event["asks"].([]interface{})[4].([]interface{})[1].(string),
+						Event["asks"].([]interface{})[5].([]interface{})[1].(string),
+						Event["asks"].([]interface{})[6].([]interface{})[1].(string),
+						Event["asks"].([]interface{})[7].([]interface{})[1].(string),
+						Event["asks"].([]interface{})[8].([]interface{})[1].(string),
+						Event["asks"].([]interface{})[9].([]interface{})[1].(string),
 					})
-				w.Flush()
-
+					w.Flush()
+				}
 			}
 		}()
 		<-doneC
@@ -105,17 +108,17 @@ func main() {
 
 /*
 func main() {
-	var filepath= "/Users/ludongdong/zaqizaba/btcdepth.csv"
-	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		fmt.Println("open file failed err:%+v\n", err)
-	}
-	defer file.Close()
-	file.WriteString("\xEF\xBB\xBF")
-	w := csv.NewWriter(file)
-	w.Write([]string{"时间","买1价","买2价","买3价","买4价","买5价","买6价","买7价","买8价","买9价","买10价","卖1价","卖2价","卖3价","卖4价","卖5价","卖6价","卖7价","卖8价","卖9价","卖10价",
-		"买1量","买2量","买3量","买4量","买5量","买6量","买7量","买8量","买9量","买10量","卖1量","卖2量","卖3量","卖4量","卖5量","卖6量","卖7量","卖8量","卖9量","卖10量"})
-	w.Flush()
+        var filepath= "/Users/ludongdong/zaqizaba/btcdepth.csv"
+        file, err := os.OpenFile(filepath, os.O_CREATE|os.O_RDWR, 0644)
+        if err != nil {
+                fmt.Println("open file failed err:%+v\n", err)
+        }
+        defer file.Close()
+        file.WriteString("\xEF\xBB\xBF")
+        w := csv.NewWriter(file)
+        w.Write([]string{"时间","买1价","买2价","买3价","买4价","买5价","买6价","买7价","买8价","买9价","买10价","卖1价","卖2价","卖3价","卖4价","卖5价","卖6价","卖7价","卖8价","卖9价","卖10价",
+                "买1量","买2量","买3量","买4量","买5量","买6量","买7量","买8量","买9量","买10量","卖1量","卖2量","卖3量","卖4量","卖5量","卖6量","卖7量","卖8量","卖9量","卖10量"})
+        w.Flush()
 
 
 
@@ -123,6 +126,4 @@ func main() {
 
 }
 
- */
-
-
+*/
