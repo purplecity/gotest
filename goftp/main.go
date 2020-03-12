397fca674bda44f9f3aafea04ff33a4e6f350f4a
likai <3308200258@qq.com>
2020-03-12 21:54:57 +0800

    Create main.go

---
+++ goftp/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jlaffaye/ftp"
+	"io/ioutil"
+	"log"
+	"time"
+)
+
+var (
+	ftpurl = "xg.gdcapi.com"
+	ftpusername = "GI5.xingyu"
+	ftppwd = "9S1gmjk65P"
+)
+func main() {
+	//连接
+	c, err := ftp.Dial(ftpurl+":21", ftp.DialWithTimeout(5*time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.Login(ftpusername, ftppwd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	namelist,err := c.NameList("/AGIN")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n",namelist)
+
+	// Do something with the FTP conn
+	r, err := c.Retr("/AGIN/20200312/202003120119.xml")
+	if err != nil {
+		panic(err)
+	}
+
+	buf, err := ioutil.ReadAll(r)
+	println(string(buf))
+
+	if err := c.Quit(); err != nil {
+		log.Fatal(err)
+	}
+	//登陆
+
+	//读取
+	//存储
+}
