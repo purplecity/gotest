71f989cf7a9233adbe9c0d5a7ae1609777a8be11
3308200258@qq.com <3308200258@qq.com>
2020-03-24 10:59:27 +0800

    Update main.go

--- hpmd5/main.go
+++ hpmd5/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	phlist := []string{"13788888880"}
-	pw :="7722777"
+	phlist := []string{"13788888888","13975368407","15173364957","15886327401","13866668888","19186803207","19186803302"}
+	pw :="qaqa2277"
 	for _, ph := range phlist {
 		data := []byte(pw)
 		has := md5.Sum(data)
